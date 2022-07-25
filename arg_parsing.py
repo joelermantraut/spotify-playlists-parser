7f56d3f613149a7b4ac288a529ca7adebca47776
Joel Ermantraut <joelermantraut@gmail.com>
2022-07-24 21:10:52 -0300

    Added show info methods

--- arg_parsing.py
+++ arg_parsing.py
@@ -24,6 +24,7 @@ class ArgParsing():
 		self.parser.add_argument("-s", "--scan", action="store_true", help="Scan all information and saves it")
 		self.parser.add_argument("-g", "--get-song", help="Show information of a song by name")
 		self.parser.add_argument("-m", "--mix-playlist", help="Mix playlist songs and saves results")
+		self.parser.add_argument("-i", "--show-info", help="Show song info")
 		self.parser.add_argument("--print-all", action="store_true", help="Print list of songs")
 
 	def parse_args(self):
@@ -49,6 +50,9 @@ class ArgParsing():
 		if args.mix_playlist:
 			self.mix_playlist(args.mix_playlist)
 
+		if args.show_info:
+			self.show_info(args.show_info)
+
 	def run_scan(self):
 		self.presentation("Total songs: " + str(len(self.spoti_parser.songs)))
 		self.save_on_file("Songs Lists", self.spoti_parser, self.spoti_parser.songs)
@@ -73,15 +77,39 @@ class ArgParsing():
 		if ID:
 			song_appearances = self.spoti_parser.get_all_song_appearances(ID)
 
-		self.presentation("Song Appearances")
+			self.presentation("Song Appearances")
 
-		for song in song_appearances:
-			print(self.spoti_parser.format_song(song))
+			for song in song_appearances:
+				print(self.spoti_parser.format_song(song))
+		else:
+			self.presentation("Song not found")
 
 	def mix_playlist(self, playlist_name):
 		new_playlist = self.spoti_parser.mix_playlist(playlist_name)
 
 		self.save_on_file("New order - " + playlist_name, self.spoti_parser, new_playlist)
+
+	def show_info(self, song_name):
+		for song in self.spoti_parser.songs:
+			if song["name"] == song_name:
+				print("Name:", song["name"])
+				print("Album:", song["album"])
+				print("Artist/s: ", ", ".join(song["artists"]))
+				print(
+					"Duration:", 
+					str(song["duration_ms"] // 60000), 
+					"Minutos,", 
+					str(round((song["duration_ms"] / 60000 - song["duration_ms"] // 60000) * 60)), 
+					"segundos."
+				)
+				print(
+					"Playlist/s:",
+					", ".join([song["playlist"]
+						for song in self.spoti_parser.get_all_song_appearances(
+							self.spoti_parser.get_song_id(song_name))]
+						)
+				)
+				print("ID:", song["id"])
 
 	def print_all(self):
 		if len(self.spoti_parser.songs):
